855770e960dbb19274a5e0183182babd7df744f3
EdelCC <edel.castillo@itexico.com>
2018-07-05 14:58:26 -0500

    Added QuizEnd

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import QuizEnd from "./QuizEnd.js";
 import QuizQuestion from "./QuizQuestion.js";
 
 let quizData = require('./quiz_data.json');
@@ -13,8 +14,16 @@ class Quiz extends Component {
   }
 
   render() {
+    const isQuizEnd = this.state.quiz_position - 1 === quizData.quiz_questions.length;
+
     return (
-      <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+      <div>
+        {
+          isQuizEnd ?
+          <QuizEnd/> :
+          <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]}/>
+        }
+      </div>
     );
   }
 }
